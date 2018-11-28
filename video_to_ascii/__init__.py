f3c3dcc2dfa5b35b82a79cf155d64819db44cfab
joel ibaceta <mail@joelibaceta.com>
2018-11-28 09:35:46 -0300

    solving error message at finish

--- video_to_ascii/__init__.py
+++ video_to_ascii/__init__.py
@@ -10,7 +10,7 @@ def play(filename):
 
     fps = 30
     time_delta = 1./fps
-    str, counter = '', 0
+    str = 0
 
     rows, columns = os.popen('stty size', 'r').read().split()
     reader, frames_count = engine.load_video_frames(filename) 
@@ -28,27 +28,28 @@ def play(filename):
         reduced_height = int(height * REDUCTION_PERCENT / 100)
 
         fill_left_with_blank = int(columns) - (reduced_width * 2) 
-
-        # read frame by frame
-        ret, frame = reader.read()
-        frame = engine.rescale_frame(frame, percent=REDUCTION_PERCENT)
         
-        for j in range(reduced_height): 
-            for i in range(reduced_width):
-                pixel = frame[j][i]
-                ascii_char = processor.pixel_to_ascii(pixel) 
-                str += ascii_char
-            str += (" " * fill_left_with_blank)
-        str += "\r\n"
+        try:
+            # read frame by frame
+            ret, frame = reader.read()
+            frame = engine.rescale_frame(frame, percent=REDUCTION_PERCENT)
+            
+            for j in range(reduced_height): 
+                for i in range(reduced_width):
+                    pixel = frame[j][i]
+                    ascii_char = processor.pixel_to_ascii(pixel) 
+                    str += ascii_char
+                str += (" " * fill_left_with_blank)
+            str += "\r\n"
 
-        t1 = time.clock()
-        delta = time_delta - (t1 - t0)
-        if (delta > 0):
-            time.sleep(delta)
-        sys.stdout.write( str )
-        str = ''
-        
-        counter += 1 
+            t1 = time.clock()
+            delta = time_delta - (t1 - t0)
+            if (delta > 0):
+                time.sleep(delta)
+            sys.stdout.write( str )
+            str = ''
+        except:
+            break
 
     file.close()
     reader.release()
