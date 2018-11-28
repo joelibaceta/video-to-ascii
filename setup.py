f9b1fcafb55782195d36f4d77c4c20f3f08ee95b
joel ibaceta <mail@joelibaceta.com>
2018-11-28 09:36:46 -0300

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="video_to_ascii",
-    version="1.0.5",
+    version="1.0.6",
     author="Joel Ibaceta",
     author_email="mail@joelibaceta.com",
     description="A simple tool to play a video using ascii characters",
