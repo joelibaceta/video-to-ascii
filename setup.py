73de932ee62aec6dee361eb3275c89214816ed80
joel ibaceta <mail@joelibaceta.com>
2018-11-30 05:55:18 -0300

    add dependencies explicit

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@ import setuptools
 
 setuptools.setup(
     name="video_to_ascii",
-    version="1.1.0",
+    version="1.1.1",
     author="Joel Ibaceta",
     author_email="mail@joelibaceta.com",
     description="A simple tool to play a video using ascii characters",
     url="https://github.com/joelibaceta/video-to-ascii",
     packages=setuptools.find_packages(),
+    install_requires=[
+          'opencv-python', 'colorize'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
