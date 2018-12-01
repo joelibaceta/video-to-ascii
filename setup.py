e00a1fea74557bfdaaadf5c2477f8be2e76d28f5
joel ibaceta <mail@joelibaceta.com>
2018-12-01 00:58:41 -0300

    allow resize and avoid distorsion for exceds width

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="video_to_ascii",
-    version="1.1.4",
+    version="1.1.5",
     author="Joel Ibaceta",
     author_email="mail@joelibaceta.com",
     description="A simple tool to play a video using ascii characters",
--- video_to_ascii/__init__.py
+++ video_to_ascii/__init__.py
@@ -10,32 +10,31 @@ def play(filename):
     fps = 30
     time_delta = 1./fps
     str = ''
-
-    rows, columns = os.popen('stty size', 'r').read().split()
     reader, frames_count = engine.load_video_frames(filename) 
 
     while(reader.isOpened()):
-        
         t0 = time.clock()
+        rows, columns = os.popen('stty size', 'r').read().split()
 
-        width = reader.get(cv2.CAP_PROP_FRAME_WIDTH)   # float
-        height = reader.get(cv2.CAP_PROP_FRAME_HEIGHT) # float
+        width = reader.get(cv2.CAP_PROP_FRAME_WIDTH)
+        height = reader.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
         REDUCTION_PERCENT = (float(rows)-1) / height * 100
 
         reduced_width = int(width * REDUCTION_PERCENT / 100)
         reduced_height = int(height * REDUCTION_PERCENT / 100)
 
-        fill_left_with_blank = int(columns) - (reduced_width * 2) 
-    
+        fill_left_with_blank = max(int(columns) - (reduced_width * 2), 0)
+        max_width = min((int(columns)/2), (reduced_width))
+
         # read frame by frame
         ret, frame = reader.read()
         frame = engine.rescale_frame(frame, percent=REDUCTION_PERCENT)
         
-        for j in range(reduced_height): 
-            for i in range(reduced_width):
+        for j in range(reduced_height):
+            for i in range(max_width):
                 pixel = frame[j][i]
-                ascii_char = processor.pixel_to_ascii(pixel) 
+                ascii_char = processor.pixel_to_ascii(pixel)
                 str += ascii_char
             str += (" " * fill_left_with_blank)
         str += "\r\n"
