65477fd09a6f5fb964ea184e7b0e495f34c03ba5
joel ibaceta <mail@joelibaceta.com>
2018-12-05 07:57:56 -0300

    add a full filled char ascci strategy

---
+++ video_to_ascii/render_strategy/ascii_color_filled_strategy.py
@@ -0,0 +1,48 @@
+"""
+This module contains a class AsciiColorStrategy, o process video frames and build an ascii output
+"""
+
+import time
+import sys
+import os
+import cv2
+
+from . import ascii_strategy as strategy
+from .. import image_processor as ipe
+
+class AsciiColorFilledStrategy(strategy.AsciiStrategy):
+    """Print each frame in the terminal using ascii characters"""
+
+    def convert_frame_pixels_to_ascii(self, frame):
+        """
+        Replace all pixeles with colored chars and return the resulting string
+
+        This method iterates each pixel of one video frame
+        respecting the dimensions of the printing area
+        to truncate the width if necessary
+        and use the pixel_to_ascii method to convert one pixel
+        into a character with the appropriate color.
+        Finally joins the set of chars in a string ready to print.
+
+        Args:
+            frame: a single video frame
+            dimensions: an array with the printing area dimensions
+                in pixels [rows, cols]
+
+        Returns:
+            str: The resulting set of colored chars as a unique string
+
+        """
+        _, cols = os.popen('stty size', 'r').read().split()
+        h, w, _ = frame.shape
+        printing_width = int(min(int(cols), (w*2))/2)
+        padding_right = max(int(cols) - (w*2), 0)
+        msg = ''
+        for j in range(h):
+            for i in range(printing_width):
+                pixel = frame[j][i]
+                colored_ascii_char = ipe.pixel_to_ascii(pixel, density=2)
+                msg += colored_ascii_char
+            msg += (" " * padding_right)
+        msg += "\r\n"
+        return msg
