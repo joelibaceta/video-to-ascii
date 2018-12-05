dfd3a99bc4b3ba130670fd934ef0ffb5246b4dc3
joel ibaceta <mail@joelibaceta.com>
2018-12-05 07:14:27 -0300

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="video_to_ascii",
-    version="1.1.7",
+    version="1.1.8",
     author="Joel Ibaceta",
     author_email="mail@joelibaceta.com",
     license='MIT',
