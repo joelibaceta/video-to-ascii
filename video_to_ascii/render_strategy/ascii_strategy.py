2588dd0c9c354f44f12d2b5feff53ce63495805d
Joel Ibaceta <joelibaceta@MacBook-de-Joel.local>
2018-12-08 11:13:09 -0300

    improve rendering

--- video_to_ascii/render_strategy/ascii_strategy.py
+++ video_to_ascii/render_strategy/ascii_strategy.py
@@ -15,7 +15,7 @@ DEFAULT_TERMINAL_SIZE = 80, 25
 class AsciiStrategy(re.RenderStrategy):
     """Print each frame in the terminal using ascii characters"""
 
-    def convert_frame_pixels_to_ascii(self, frame, dimensions=DEFAULT_TERMINAL_SIZE, include_new_line_chars=False):
+    def convert_frame_pixels_to_ascii(self, frame, dimensions=DEFAULT_TERMINAL_SIZE, new_line_chars=False):
         """
         Replace all pixeles with colored chars and return the resulting string
 
@@ -30,7 +30,7 @@ class AsciiStrategy(re.RenderStrategy):
             frame: a single video frame
             dimensions: an array with the printing area dimensions
                 in pixels [rows, cols]
-            include_new_line_chars: if should append a new line character
+            new_line_chars: if should append a new line character
                 at end of each row
 
         Returns:
@@ -39,24 +39,30 @@ class AsciiStrategy(re.RenderStrategy):
         """
         cols, _ = dimensions
         h, w, _ = frame.shape
+
         printing_width = int(min(int(cols), (w*2))/2)
+        pad = max(int(cols) - printing_width*2, 0) 
+         
+        
         msg = ''
-        for j in range(h):
+        for j in range(h-1):
             for i in range(printing_width):
                 pixel = frame[j][i]
                 msg += self.apply_pixel_to_ascii_strategy(pixel)
-            if include_new_line_chars:
+            if new_line_chars:
                 msg += "\n"
+            else:
+                msg += " " * (pad)
         msg += "\r\n"
         return msg
 
     def apply_pixel_to_ascii_strategy(self, pixel):
         return ipe.pixel_to_ascii(pixel)
-    
+
     def apply_end_line_modifier(self, msg):
         return msg
 
-    def render(self, vc, output=None):
+    def render(self, cap, output=None):
         """
         Iterate each video frame to print a set of ascii chars
 
@@ -69,12 +75,15 @@ class AsciiStrategy(re.RenderStrategy):
         with the fps expected (30 fps by default).
 
         Args:
-            vc: An OpenCV video capture
+            cap: An OpenCV video capture
+            output: If the render should be exported to a bash file
         """
 
-        v_width = vc.get(cv2.CAP_PROP_FRAME_WIDTH)
-        v_height = vc.get(cv2.CAP_PROP_FRAME_HEIGHT)
-        length = vc.get(cv2.CAP_PROP_FRAME_COUNT)
+        v_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+        v_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        length = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        
 
         if output is not None:
             file = open(output, 'w+')
@@ -82,44 +91,44 @@ class AsciiStrategy(re.RenderStrategy):
             file.write("echo -en '\033[2J' \n")
             file.write("echo -en '\u001b[0;0H' \n")
 
-        fps = 30
         time_delta = 1./fps
         counter=0
+        sys.stdout.write("echo -en '\033[2J' \n")
         # read each frame
-        while(vc.isOpened()):
+        while cap.isOpened():
             t0 = time.clock()
             rows, cols = os.popen('stty size', 'r').read().split()
-            _ret, frame = vc.read()
-            if (frame is None):
+            _ret, frame = cap.read()
+            if frame is None:
                 break
             # sleep if the process was too fast
             if output is None:
+                sys.stdout.write('\u001b[0;0H')
                 # scale each frame according to terminal dimensions
                 resized_frame = self.resize_frame(frame, (cols, rows))
                 # convert frame pixels to colored string
-                str = self.convert_frame_pixels_to_ascii(resized_frame, (cols, rows))
-                sys.stdout.write('\u001b[0;0H')
+                msg = self.convert_frame_pixels_to_ascii(resized_frame, (cols, rows)) 
                 t1 = time.clock()
                 delta = time_delta - (t1 - t0)
-                if (delta > 0):
+                if delta > 0:
                     time.sleep(delta)
-                sys.stdout.write(str) # Print the final string
+                sys.stdout.write(msg) # Print the final string
             else:
                 print(self.build_progress(counter, length))
                 print("\u001b[2A")
                 resized_frame = self.resize_frame(frame)
-                str = self.convert_frame_pixels_to_ascii(resized_frame, include_new_line_chars=True)
+                msg = self.convert_frame_pixels_to_ascii(resized_frame, new_line_chars=True)
                 file.write("sleep 0.033 \n")
-                file.write("echo -en '" + str + "'" + "\n" ) 
+                file.write("echo -en '" + msg + "'" + "\n" ) 
                 file.write("echo -en '\u001b[0;0H' \n")
             counter += 1
-    
+        sys.stdout.write("echo -en '\033[2J' \n")
+
     def build_progress(self, progress, total):
- 
+        """Build a progress bar in the terminal"""
         progress_percent =  int(progress / total * 100) 
         adjusted_size_percent = int((20 / 100) * progress_percent) 
         progress_bar = ('█' * adjusted_size_percent) + ('░' * (20-adjusted_size_percent))
-        
         return  "  " +  "|" +  progress_bar + "| " + str(progress_percent) + "%"
 
     def resize_frame(self, frame, dimensions=DEFAULT_TERMINAL_SIZE):
@@ -127,20 +136,21 @@ class AsciiStrategy(re.RenderStrategy):
         Resize a frame to meet the terminal dimensions
 
         Calculating the output terminal dimensions (cols, rows),
-        we can to get a reduction factor to resize the frame 
-        according to the height of the terminal mainly 
+        we can to get a reduction factor to resize the frame
+        according to the height of the terminal mainly
         to print each frame at a time, using all the available rows
 
         Args:
             frame: Frame to resize
+            dimensions: If you want to set a printer area size (cols, rows)
         Returns:
             A resized frame
         """
-        h, w, _ = frame.shape
-        _, rows= dimensions
-        reduction_factor = (float(rows)-1) / h * 100
-        reduced_width = int(w * reduction_factor / 100)
-        reduced_height = int(h * reduction_factor / 100)
+        height, width, _ = frame.shape
+        _, rows = dimensions
+        reduction_factor = (float(rows)) / height * 100
+        reduced_width = int(width * reduction_factor / 100)
+        reduced_height = int(height * reduction_factor / 100)
         dimension = (reduced_width, reduced_height)
         resized_frame = cv2.resize(frame, dimension, interpolation=cv2.INTER_LINEAR)
         return resized_frame
--- video_to_ascii/render_strategy/image_processor.py
+++ video_to_ascii/render_strategy/image_processor.py
@@ -1,5 +1,7 @@
-from xtermcolor import colorize
+"""Module with usefull functions to image processing"""
+
 import colorsys
+from xtermcolor import colorize
 
 CHARS_LIGHT = [' ', ' ', '.', ':', '!', '+', '*', 'e', '$', '@', '8']
 CHARS_COLOR = ['.', '*', 'e', 's', '◍']
@@ -13,7 +15,7 @@ def brightness_to_ascii(i, density=0):
     """
     chars_collection = DENSITY[density]
     size = len(chars_collection) - 1
-    index = int(size * i / 255) #int((i / 255) * size) 
+    index = int(size * i / 255)
     return chars_collection[index]
 
 def colorize_char(char, ansi_color):
@@ -27,21 +29,28 @@ def pixel_to_ascii(pixel, colored=True, density=0):
     """
     Convert a pixel to char
     """
-    b, g, r = pixel[0], pixel[1], pixel[2]
+    bgr = tuple(float(x) for x in pixel[:3])
+    rgb = tuple(reversed(bgr))
     char = ''
     if colored:
-        h, s, v = colorsys.rgb_to_hsv(float(r), float(g), float(b))
-        bright = rgb_to_brightness(r, g, b)
-        s = s + 0.3 if s + 0.3 < 1.0 else 1.0
-        r, g, b = colorsys.hsv_to_rgb(h, s, v)
+        bright = rgb_to_brightness(*rgb)
+        rgb = increase_saturation(*rgb)
         char = brightness_to_ascii(bright, density)
-        ansi_color = rgb_to_ansi(r, g, b)
+        ansi_color = rgb_to_ansi(*rgb)
         char = colorize(char*2, ansi=ansi_color)
     else:
-        bright = rgb_to_brightness(r, g, b, grayscale=True)
+        bright = rgb_to_brightness(*rgb, grayscale=True)
         char = brightness_to_ascii(bright, density)
         char = char*2
     return char
+
+def increase_saturation(r, g, b):
+    """
+    Increase the saturation from rgb and return the new value as rgb tuple
+    """
+    h, s, v = colorsys.rgb_to_hsv(r, g, b)
+    s = min(s+0.3, 1.0)
+    return colorsys.hsv_to_rgb(h, s, v)
 
 def rgb_to_brightness(r, g, b, grayscale=False):
     """
@@ -56,17 +65,17 @@ def rgb_to_ansi(r, g, b):
     """
     Convert an rgb color to ansi color
     """
-    r, g, b = int(r), int(g), int(b)
-    if (r == g & g == b):
-        if (r < 8):
+    (r, g, b) = int(r), int(g), int(b)
+    if r == g & g == b:
+        if r < 8:
             return int(16)
-        if (r > 248):
+        if r > 248:
             return int(230)
         return int(round(((r - 8) / 247) * 24) + 232)
-    r_in_ansi_range = int(round(float(r) / 51))
-    g_in_ansi_range = int(round(float(g) / 51))
-    b_in_ansi_range = int(round(float(b) / 51))
-    ansi = 16 + (36 * r_in_ansi_range) + (6 * g_in_ansi_range) + b_in_ansi_range
-    return int(ansi)
 
-    
+    to_ansi_range = lambda a: int(round(a / 51.0))
+    r_in_range = to_ansi_range(r)
+    g_in_range = to_ansi_range(g)
+    b_in_range = to_ansi_range(b)
+    ansi = 16 + (36 * r_in_range) + (6 * g_in_range) + b_in_range
+    return int(ansi)
